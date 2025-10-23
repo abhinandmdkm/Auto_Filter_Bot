211901775e60c10cffb2341eb80dd15e23d50ee4
abhinandmdkm <abhinandmdkm@gmail.com>
2025-10-24 00:25:40 +0530

    Update group and owner links in info.py

--- info.py
+++ info.py
@@ -120,8 +120,8 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+DwrVzb3ix35mNWY1') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+DwrVzb3ix35mNWY1') # Owner link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinemayugam') # Group link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/cinemayugam') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/cinemayugam') # Update channel link for the bot
 
 # ============================
