8c1f94e4d3be613d4bb4965aedb35da2e788c709
abhinandmdkm <abhinandmdkm@gmail.com>
2025-10-28 14:25:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,8 +53,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-100')  # Request channel id (m
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+uqQWzphDpeA4ZjVl')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1001970049977 -4881675966")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-4881675966 -1003107856581 -1003080058336")# Channels for force sub (make sure bot is admin)
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-4881675966")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_channels     = environ.get("AUTH_CHANNELS", "-1003080058336")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
