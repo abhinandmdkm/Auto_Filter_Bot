254319a997b32cf6ed000336936ef959386da919
abhinandmdkm <abhinandmdkm@gmail.com>
2025-11-08 09:32:41 +0530

    Update default value for REQST_CHANNEL_ID

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1003009256040'))  # Bin channel i
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1003009256040'))  # Premium logs channel id
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1003183381750').split()] #(make sure bot is admin)
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-4881675966')  # Support group id (make sure bot is admin)
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001970049977')  # Request channel id (make sure bot is admin)
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-100')  # Request channel id (make sure bot is admin)
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+uqQWzphDpeA4ZjVl')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
