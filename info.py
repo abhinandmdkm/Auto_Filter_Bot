257dd7c5dc3118014641b212d70e55d0e279dfec
abhinandmdkm <abhinandmdkm@gmail.com>
2025-12-03 17:27:06 +0530

    Change sample picture URLs in info.py
    Updated sample picture URLs in the PICS variable.

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when i
 #Making it false will not save caption in db SO you can save some storage space
 
 
-PICS = (environ.get('PICS', 'https://envs.sh/N3z.mp4 https://envs.sh/N3p.mp4')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://envs.sh/kYs.jpg https://envs.sh/kYN.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
